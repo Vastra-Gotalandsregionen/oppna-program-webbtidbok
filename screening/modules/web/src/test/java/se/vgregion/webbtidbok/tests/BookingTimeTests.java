a42955a8590b8e2f9e4e64254c2745bbaa78978c
conny pemfors <conny.pemfors@create.se>
2010-04-28 12:11:11 +0000

    add bokingtimes update.xhtml.

--- screening/modules/web/src/test/java/se/vgregion/webbtidbok/tests/BookingTimeTests.java
+++ screening/modules/web/src/test/java/se/vgregion/webbtidbok/tests/BookingTimeTests.java
@@ -25,12 +25,14 @@ import org.junit.*;
 
 
 import se.vgregion.webbtidbok.*;
+import se.vgregion.webbtidbok.lang.DateHandler;
 import se.vgregion.webbtidbok.ws.ArrayOfBookingPlace;
 import se.vgregion.webbtidbok.ws.BookingPlace;
 import se.vgregion.webbtidbok.ws.BookingRequest;
 import se.vgregion.webbtidbok.ws.ObjectFactory;
 
 import java.util.*;
+import java.util.Calendar;
 
 import javax.xml.bind.JAXBElement;
 
@@ -64,6 +66,245 @@ public class BookingTimeTests {
 	}
 
 	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Test
+	public void testBookingTimeLocals(){
+		
+		Calendar tCal = Calendar.getInstance();
+		tCal.set(Calendar.MONTH, 4);
+		tCal.set(Calendar.DATE, 12);
+		
+		System.out.println(tCal.getTime().toString());
+		
+		BookingService service = new BookingService();
+		
+		State credentials = new State();
+		//credentials.setPasswd("Zs12JzIW");
+		//credentials.setPnr("19121212-1212");
+		credentials.setPasswd("4YL7CXnp");
+		credentials.setPnr("19121212-1212");
+		
+		credentials.setCentralTidbokID(1);
+		credentials.setSelectedDate(tCal);
+		credentials.setLoggedIn(true);
+		
+		JAXBElement<String> fromDat = objectFactory.createBookingRequestFromDat("2010-05-12");
+		
+		BookingRequest request = ws.getQueryWSRequest(credentials);
+		request.setFromDat(fromDat);
+		request.setCentralTidbokID(1);
+		
+		  
+		//List<BookingTime> timeList = time.getBookingTime();
+		List<BookingTimeLocal> timeList = service.getBookingTime(credentials);
+		
+		if(timeList == null){
+			Assert.assertFalse(true);
+			
+		}
+		else{
+			
+			if(timeList.isEmpty()){
+				Assert.assertFalse(true);
+			}
+			else{
+				
+				for(BookingTimeLocal bt : timeList){
+					//System.out.println(bp.getAddress().getValue());
+					//System.out.println(bp.getCentralTidbokID());
+					//System.out.println(bp.getMottagning().getValue());
+					System.out.println("Antal: "  + bt.toString());
+					
+				}
+				
+				
+				
+			}
+			
+			
+			Assert.assertTrue(true);
+		}
+		
+	}
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Test
+	public void testMultipleBookingTimeCalendars(){
+		/*
+		fje5rnXG	19910104-2399
+		NC1dqBZa	19030311-9804
+		nDbUMxTL	19700123-9297
+		4YL7CXnp	19121212-1212
+		6wHRDtKa	19910104-2399
+		kzxpQlLb	19420213-8014
+		*/
+		
+		State credentials = new State();
+		//credentials.setPasswd("Zs12JzIW");
+		//credentials.setPnr("19121212-1212");
+		credentials.setPasswd("fje5rnXG");
+		credentials.setPnr("19910104-2399");
+		
+		testBookingTimeCalendar2();
+		
+		credentials.setPasswd("NC1dqBZa");
+		credentials.setPnr("19030311-9804");
+		
+		testBookingTimeCalendar2();
+		
+		credentials.setPasswd("nDbUMxTL");
+		credentials.setPnr("19700123-9297");
+		
+		testBookingTimeCalendar2();
+		
+		credentials.setPasswd("4YL7CXnp");
+		credentials.setPnr("19121212-1212");
+		
+		testBookingTimeCalendar2();
+		
+		credentials.setPasswd("6wHRDtKa");
+		credentials.setPnr("19910104-2399");
+		
+		testBookingTimeCalendar2();
+		
+		credentials.setPasswd("kzxpQlLb");
+		credentials.setPnr("19420213-8014");
+		
+		testBookingTimeCalendar2();
+	}
+	
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Test
+	public void testBookingTimeCalendar2(){
+		
+		Calendar tCal = Calendar.getInstance();
+		tCal.set(Calendar.MONTH, 4);
+		tCal.set(Calendar.DATE, 14);
+		
+		System.out.println(tCal.getTime().toString());
+		
+		BookingService service = new BookingService();
+		
+		State credentials = new State();
+		//credentials.setPasswd("Zs12JzIW");
+		//credentials.setPnr("19121212-1212");
+		credentials.setPasswd("kzxpQlLb");
+		credentials.setPnr("19420213-8014");
+		credentials.setLoggedIn(true);
+		credentials.setCentralTidbokID(1);
+		JAXBElement<String> fromDat = objectFactory.createBookingRequestFromDat("2010-05-30");
+		
+		
+		//request.setFromDat(fromDat);
+		//request.setCentralTidbokID(1);
+		
+		BookingResponseLocal bookingResponseLocal = service.getBooking(credentials);
+		System.out.println("bookingResponseLocal.state.calendardate: " + DateHandler.setCalendarDateFormat(credentials.getSelectedDate()));
+		System.out.println("CentralTidBokId: " + credentials.getCentralTidbokID());
+		//List<BookingTime> timeList = time.getBookingTime();
+		List<BookingTimeLocal> timeList = service.getBookingTime(credentials);
+		
+		if(timeList == null){
+			Assert.assertFalse(true);
+			
+		}
+		else{
+			
+			if(timeList.isEmpty()){
+				Assert.assertFalse(true);
+				
+			}
+			else{
+				
+				for(BookingTimeLocal bt : timeList){
+					//System.out.println(bp.getAddress().getValue());
+					//System.out.println(bp.getCentralTidbokID());
+					//System.out.println(bp.getMottagning().getValue());
+					System.out.println("Antal: "  + bt.toString());
+					
+				}
+				
+				
+				
+			}
+			
+			
+			Assert.assertTrue(true);
+			
+		}
+		
+	}
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Test
+	public void testBookingTimeCalendar(){
+		
+		Calendar tCal = Calendar.getInstance();
+		tCal.set(Calendar.MONTH, 4);
+		tCal.set(Calendar.DATE, 14);
+		
+		System.out.println(tCal.getTime().toString());
+		
+		BookingService service = new BookingService();
+		
+		State credentials = new State();
+		//credentials.setPasswd("Zs12JzIW");
+		//credentials.setPnr("19121212-1212");
+		credentials.setPasswd("kzxpQlLb");
+		credentials.setPnr("19420213-8014");
+		
+		credentials.setLoggedIn(true);
+		
+		JAXBElement<String> fromDat = objectFactory.createBookingRequestFromDat("2010-05-30");
+		
+		
+		//request.setFromDat(fromDat);
+		//request.setCentralTidbokID(1);
+		
+		BookingResponseLocal bookingResponseLocal = service.getBooking(credentials);
+		System.out.println("bookingResponseLocal.state.calendardate: " + DateHandler.setCalendarDateFormat(credentials.getSelectedDate()));
+		System.out.println("CentralTidBokId: " + credentials.getCentralTidbokID());
+		//List<BookingTime> timeList = time.getBookingTime();
+		List<BookingTimeLocal> timeList = service.getBookingTime(credentials);
+		
+		if(timeList == null){
+			Assert.assertFalse(true);
+			
+		}
+		else{
+			
+			if(timeList.isEmpty()){
+				Assert.assertFalse(true);
+			}
+			else{
+				
+				for(BookingTimeLocal bt : timeList){
+					//System.out.println(bp.getAddress().getValue());
+					//System.out.println(bp.getCentralTidbokID());
+					//System.out.println(bp.getMottagning().getValue());
+					System.out.println("Antal: "  + bt.toString());
+					
+				}
+				
+				
+				
+			}
+			
+			
+			Assert.assertTrue(true);
+		}
+		
+	}
+	
 	
 	/**
 	 * @throws java.lang.Exception
@@ -73,10 +314,10 @@ public class BookingTimeTests {
 		State credentials = new State();
 		//credentials.setPasswd("Zs12JzIW");
 		//credentials.setPnr("19121212-1212");
-		credentials.setPasswd("Y8PBZRUr");
-		credentials.setPnr("19960103-2395");
+		credentials.setPasswd("fje5rnXG");
+		credentials.setPnr("19910104-2399");
 		
-		JAXBElement<String> fromDat = objectFactory.createBookingRequestFromDat("2010-03-31");
+		JAXBElement<String> fromDat = objectFactory.createBookingRequestFromDat("2010-05-14");
 		JAXBElement<String> toDat = objectFactory.createBookingRequestToDat("2010-05-31");
 		
 		
