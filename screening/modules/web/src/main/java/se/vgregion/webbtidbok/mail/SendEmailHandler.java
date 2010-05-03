cb8bb3b891bb70883ffab3a7f2af8e2e8d7a7eab
conny pemfors <conny.pemfors@create.se>
2010-05-03 10:51:07 +0000

    adding email message sending method to recall booking.

--- screening/modules/web/src/main/java/se/vgregion/webbtidbok/mail/SendEmailHandler.java
+++ screening/modules/web/src/main/java/se/vgregion/webbtidbok/mail/SendEmailHandler.java
@@ -31,6 +31,7 @@ import java.util.Properties;
 import java.util.*;
 import java.io.Serializable;
 import javax.mail.Address;
+import se.vgregion.webbtidbok.*;
 
 
 
@@ -87,7 +88,7 @@ public class SendEmailHandler implements Serializable {
 	         //String server = "smtp.knowit.se";
 			String server = "smtp.gmail.com";
 			String from= "DoNotReply@knowit.se";
-			String userName="carl.stromhielm@gmail.com";
+			String userName="";
 			String password=""; //add TODO: password for smtpserver
 	         
 	         String subject="Tiden är avbokad"; 
@@ -144,6 +145,90 @@ public class SendEmailHandler implements Serializable {
 		}
 	}
 	
+	
+	
+	public void sendMailTotalRecallBooking(String sendMailTo, BookingResponseLocal response){
+		try{
+			
+			
+			 //Properties properties = getProperties();
+			
+			 //String server= (String) properties.get("server.host"); 
+	         //String from= (String) properties.get("server.fromemail"); 
+	         
+	         //String server = "smtp.knowit.se";
+			String server = "smtp.gmail.com";
+			String from= "DoNotReply@knowit.se";
+			String userName="";
+			String password=""; //add TODO: password for smtpserver
+	         
+	        String subject="Din tid för undersökning av stora kroppspulsådern är ombokad."; 
+	        String messageBody="<html>";
+	        messageBody = messageBody + "<body>";
+	        messageBody = messageBody + "<h2>" + "Din tid för undersökning av stora kroppspulsådern är ombokad." + "</h2>" + "<br/>";
+	        messageBody = messageBody + "<p>" + "Person: " +  response.getDisplayName() +  " " + response.getPnr() +  "</p>" + "<br/>";
+	        messageBody = messageBody + "<p>" + "Var: " +  response.getMainSector() +  " " + response.getAddress() + "</p>" + "<br/>";
+	        messageBody = messageBody + "<p>" + "När: " +  response.getTimeBookingString()  + "</p>" + "<br/>";
+	        
+	        
+	        messageBody = messageBody + "</body>";
+	        messageBody = messageBody + "</html>";
+	        
+	        System.out.println(messageBody);
+	        
+	        System.out.println("send mail to: " + sendMailTo );
+	             
+	        //String mailServer, String from, String to,String subject, String messageBody,String[] attachments
+	            
+			Properties props = System.getProperties(); 
+			props.put("mail.smtp.host", server); 
+			
+			
+			//user authentication with gmail.
+			props.put("mail.smtp.user", userName); 
+			props.put("mail.smtp.password", password); 
+			props.put("mail.smtp.port", "465"); // 587 is the port number of yahoo mail 
+			props.put("mail.smtp.auth", "true"); 
+
+			
+			
+			
+			// Get a mail session 
+			Session session = Session.getDefaultInstance(props, null); 
+
+			// Define a new mail message 
+			Message message = new MimeMessage(session); 
+			message.setFrom(new InternetAddress(from)); 
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(sendMailTo)); 
+			message.setSubject(subject); 
+
+			// Create a message part to represent the body text 
+			BodyPart messageBodyPart = new MimeBodyPart(); 
+			messageBodyPart.setText(messageBody); 
+			
+			//use a MimeMultipart as we need to handle the file attachments 
+			Multipart multipart = new MimeMultipart(); 
+
+			//add the message body to the mime message 
+			multipart.addBodyPart(messageBodyPart); 
+
+			// add any file attachments to the message 
+			//addAttachments(attachments, multipart); 
+
+			// Put all message parts in the message 
+			message.setContent(multipart); 
+			message.setContent(message, "text/html");
+
+			// Send the message 
+			Transport.send(message); 
+			
+			
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			
+		}
+	}
 	
 	
 	public void sendMail(EmailStringHandler emailStringHandler){
@@ -203,6 +288,114 @@ public class SendEmailHandler implements Serializable {
 	}
 	
 	
+	
+	public void sendMailTotalRecallBooking(EmailStringHandler emailStringHandler, BookingResponseLocal response){
+		try{
+			
+			
+			 //Properties properties = getProperties();
+			
+			 //String server= (String) properties.get("server.host"); 
+	         //String from= (String) properties.get("server.fromemail"); 
+	         
+	         String server = "smtp.knowit.se";
+			//String server = "smtp.gmail.com";
+			String from= "DoNotReply@knowit.se";
+			String userName="";
+			String password=""; //add TODO: password for smtpserver
+	         
+	        String subject="Din tid för undersökning av stora kroppspulsådern är ombokad."; 
+	        String messageBody="<html>";
+	        messageBody = messageBody + "<body>";
+	        messageBody = messageBody + "<h2>" + "Din tid för undersökning av stora kroppspulsådern är ombokad." + "</h2>" + "<br/>";
+	        messageBody = messageBody + "<p>" + "Person: " +  response.getDisplayName() +  " " + response.getPnr() +  "</p>" + "<br/>";
+	        messageBody = messageBody + "<p>" + "Var: " +  response.getMainSector() +  " " + response.getAddress() + "</p>" + "<br/>";
+	        messageBody = messageBody + "<p>" + "När: " +  response.getTimeBookingString()  + "</p>" + "<br/>";
+	        
+	        
+	        messageBody = messageBody + "</body>";
+	        messageBody = messageBody + "</html>";
+	        
+	        System.out.println(messageBody);
+	        
+	        System.out.println("send mail to: " + emailStringHandler.getEmail() );
+	             
+	        //String mailServer, String from, String to,String subject, String messageBody,String[] attachments
+	            
+			Properties props = System.getProperties(); 
+			props.put("mail.smtp.host", server); 
+			
+			
+			//user authentication with gmail.
+			//props.put("mail.smtp.user", userName); 
+			//props.put("mail.smtp.password", password); 
+			//props.put("mail.smtp.port", "465"); // 587 is the port number of yahoo mail 
+			//props.put("mail.smtp.auth", "true"); 
+
+			
+			
+			
+			// Get a mail session 
+			Session session = Session.getDefaultInstance(props, null); 
+
+			// Define a new mail message 
+			Message message = new MimeMessage(session); 
+			message.setFrom(new InternetAddress(from)); 
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(emailStringHandler.getEmail())); 
+			message.setSubject(subject); 
+
+			// Create a message part to represent the body text 
+			BodyPart messageBodyPart = new MimeBodyPart(); 
+			messageBodyPart.setText(messageBody); 
+			
+			//use a MimeMultipart as we need to handle the file attachments 
+			Multipart multipart = new MimeMultipart(); 
+
+			//add the message body to the mime message 
+			multipart.addBodyPart(messageBodyPart); 
+
+			// add any file attachments to the message 
+			//addAttachments(attachments, multipart); 
+
+			
+			
+			
+			
+			MimeMultipart content = new MimeMultipart();
+			MimeBodyPart text = new MimeBodyPart();
+			MimeBodyPart html = new MimeBodyPart();
+			          
+			text.setText( subject );
+			text.setHeader("MIME-Version" , "1.0" );
+			text.setHeader("Content-Type" , text.getContentType() );
+			          
+			html.setContent(messageBody, "text/html");
+			html.setHeader("MIME-Version" , "1.0" );
+			html.setHeader("Content-Type" , html.getContentType() );
+
+			content.addBodyPart(text);
+			content.addBodyPart(html);
+			          
+			message.setContent( content );
+			message.setHeader("MIME-Version" , "1.0" );
+			message.setHeader("Content-Type" , content.getContentType() );
+			message.setHeader("X-Mailer", "Recommend-It Mailer V2.03c02");
+			message.setSentDate(new Date());
+			
+			
+			// Put all message parts in the message 
+			//message.setContent(content); 
+			//message.setContent(message, "text/html");
+
+			// Send the message 
+			Transport.send(message); 
+			
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			
+		}
+	}
 	
 	
 	
