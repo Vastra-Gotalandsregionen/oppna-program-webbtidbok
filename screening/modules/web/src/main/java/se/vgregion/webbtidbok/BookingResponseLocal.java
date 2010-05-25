9fba4424498afea46b1ddd624c1c716c7247d39f
krizsan.ivan <krizsan.ivan@localhost>
2010-05-25 13:11:02 +0000

    Fixed display of client names so that first name(s) is displayed followed by last name(s).

--- screening/modules/web/src/main/java/se/vgregion/webbtidbok/BookingResponseLocal.java
+++ screening/modules/web/src/main/java/se/vgregion/webbtidbok/BookingResponseLocal.java
@@ -25,199 +25,180 @@ import se.vgregion.webbtidbok.lang.StringHandler;
 import se.vgregion.webbtidbok.ws.BookingResponse;
 
 public class BookingResponseLocal implements Serializable {
-	
-	private static final long serialVersionUID = 1L;
-	/*
-	Pnr	String	YYYMMdd-nnnn
-	Namn	String	Patientens namn
-	Vardgivare	String	V�rdgivare, klar text
-	Mottgning	string	
-	Adress	String	
-	BokadTid	DateTime	Yyyy-MM-dd HH:mm
-	CentralTidbokid		
-	MobilTel	String	
-	Epost	string	
-	AntalOmbok	int	Antal gjorda ombokningar
-	MaxAntalOmbok	int	Max antal till�tna ombokningar
-	Place	BookingPlace	
-	*/
-	private String pnr;
-	private String name;
-	private String localDoctor;
-	private String address;
-	private Date timeBooking;
-	private String timeBookingString;
-	private String vTeam;
-	private String mainSector;
-	private String mobilePhone;
-	private String email;
-	private int numberBookings;
-	private int maxNbrBookings = 2;
-	private String displayName;
-	private int centralTimeBookingId = 0;
-		
-	public BookingResponseLocal() {
-		// TODO  Auto-generated constructor stub
-	}
-	
-	@SuppressWarnings("deprecation")
-	public BookingResponseLocal(BookingResponse response){
-		
-		setPnr(response.getPnr().getValue());
-		setName(response.getNamn().getValue());
-		setDisplayName(response.getNamn().getValue());
-		setLocalDoctor(response.getVardgivare().getValue());
-		setAddress(response.getAddress().getValue()); 
-		setTeam(response.getExternalID().getValue());
-		setMainSector(response.getMottagning().getValue());
-		setMobilePhone(response.getMobilTel().getValue());
-		setEmail(response.getEpost().getValue());
-		this.setNumberOfBookings(response.getAntalOmbok());
-		this.setMaxNbrBookings(response.getMaxAntalOmbok());
-		this.setCentralTimeBookId(response.getCentralTidbokID());
-		
-		//Set time booking
-		timeBooking = new Date();
-		//set hours, minutes
-		timeBooking.setHours(response.getBokadTid().getHour());
-		timeBooking.setMinutes(response.getBokadTid().getMinute());
-		timeBooking.setSeconds(response.getBokadTid().getSecond());
-		timeBooking.setMonth(response.getBokadTid().getMonth() - 1);
-		timeBooking.setYear(response.getBokadTid().getYear() - 1900);
-		timeBooking.setDate(response.getBokadTid().getDay());
-		setTimeBookingString(DateHandler.setLocaleString(timeBooking));
-		setTimeBookingString(StringHandler.toFirstLetterToUpperCase(getTimeBookingString()));		
-	}
-	
-	public void setCentralTimeBookId(int id){
-		centralTimeBookingId = id;
-	}
-	
-	public int getCentralTimeBookId(){
-		return centralTimeBookingId;
-	}
-	
-	public void setDisplayName(String name) {
-		displayName = StringHandler.capitalizeName(name);
 
-		System.out.println("displayname: " + displayName);
-	}
+  private static final long serialVersionUID = 1L;
 
-	public void setPnr(String pnr){
-		this.pnr = pnr;
-	}
-	
-	public void setName(String name){
-		this.name = name;
-	}
-	
-	public void setLocalDoctor(String doctor){
-		this.localDoctor = doctor;
-	}
-	
-	public void setAddress(String address){
-		this.address = address;
-	}
-	
-	public void setTimeBooking(Date tBooking){
-		this.timeBooking = tBooking;
-	}
-	
-	public void setTimeBookingString(String sBooking){
-		this.timeBookingString = sBooking;
-	}
-	
-	public void setTeam(String t){
-		this.vTeam = t;
-	}
-	
-	public void setMainSector(String sector){
-		this.mainSector = sector;
-	}
-	
-	public void setMobilePhone(String mPhone){
-		this.mobilePhone = mPhone;
-	}
-	
-	public void setEmail(String email){
-		this.email = email;
-	}	
-	
-	public void setNumberOfBookings(int nbrBookings){
-		this.numberBookings = nbrBookings;
-	}
-	
-	public void setMaxNbrBookings(int maxBookings){
-		this.maxNbrBookings = maxBookings;
-	}
+  private String pnr;
+  private String name;
+  private String localDoctor;
+  private String address;
+  private Date timeBooking;
+  private String timeBookingString;
+  private String vTeam;
+  private String mainSector;
+  private String mobilePhone;
+  private String email;
+  private int numberBookings;
+  private int maxNbrBookings = 2;
+  private String displayName;
+  private int centralTimeBookingId = 0;
 
-	public String getPnr(){
-		return pnr;
-	}
-	
-	public String getName(){
-		return name;
-	}
-	
-	public String getDisplayName() {
-		return displayName;
-	}
-	
-	public String getLocalDoctor(){
-		return this.localDoctor;
-	}
-	
-	public String getAddress(){
-		return address;
-	}
-	
-	public Date getTimeBooking(){
-		return this.timeBooking;
-	}
-	
-	public String getTimeBookingString(){
-		return this.timeBookingString;
-	}
-	public String getTeam(){
-		return this.vTeam;
-	}
-	
-	public String getMainSector(){
-		
-		return mainSector;
-	}
-	
-	public String getMobilePhone(){
-		return mobilePhone;
-	}
-	
-	public String getEmail(){
-		return this.email;
-	}	
-	
-	public int getNumberOfBookings(){
-		return this.numberBookings;
-	}
-	
-	public int getMaxNbrBookings(){
-		return this.maxNbrBookings;
-	}
-	
-	public boolean getIsDisabled() {
-		return numberBookings == maxNbrBookings;
-	}
-	
-	public String toString(){
-		return  "pnr " + pnr +
-			" name " + name +
-			" doctor " + localDoctor +
-			" address " + address +
-			" booking " + timeBooking +
-			" team " + vTeam +
-			" main sector " + mainSector +
-			" mobilephone " + mobilePhone +
-			" email " + email +
-			" numberBookings " + numberBookings +
-			" maxNbrBookings " + maxNbrBookings;
-	}
+  public BookingResponseLocal() {
+  }
+
+  @SuppressWarnings("deprecation")
+  public BookingResponseLocal(BookingResponse response) {
+
+    setPnr(response.getPnr().getValue());
+    setName(response.getNamn().getValue());
+    setDisplayName(response.getNamn().getValue());
+    setLocalDoctor(response.getVardgivare().getValue());
+    setAddress(response.getAddress().getValue());
+    setTeam(response.getExternalID().getValue());
+    setMainSector(response.getMottagning().getValue());
+    setMobilePhone(response.getMobilTel().getValue());
+    setEmail(response.getEpost().getValue());
+    this.setNumberOfBookings(response.getAntalOmbok());
+    this.setMaxNbrBookings(response.getMaxAntalOmbok());
+    this.setCentralTimeBookId(response.getCentralTidbokID());
+
+    // Set time booking
+    timeBooking = new Date();
+    // set hours, minutes
+    timeBooking.setHours(response.getBokadTid().getHour());
+    timeBooking.setMinutes(response.getBokadTid().getMinute());
+    timeBooking.setSeconds(response.getBokadTid().getSecond());
+    timeBooking.setMonth(response.getBokadTid().getMonth() - 1);
+    timeBooking.setYear(response.getBokadTid().getYear() - 1900);
+    timeBooking.setDate(response.getBokadTid().getDay());
+    setTimeBookingString(DateHandler.setLocaleString(timeBooking));
+    setTimeBookingString(StringHandler.toFirstLetterToUpperCase(getTimeBookingString()));
+  }
+
+  public void setCentralTimeBookId(int id) {
+    centralTimeBookingId = id;
+  }
+
+  public int getCentralTimeBookId() {
+    return centralTimeBookingId;
+  }
+
+  public void setDisplayName(String name) {
+    displayName = StringHandler.capitalizeName(name);
+    String theNames[] = displayName.split(",");
+    if (theNames.length == 2) {
+      displayName = theNames[1].trim() + " " + theNames[0].trim();
+    }
+    System.out.println("displayname: " + displayName);
+  }
+
+  public void setPnr(String pnr) {
+    this.pnr = pnr;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setLocalDoctor(String doctor) {
+    this.localDoctor = doctor;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public void setTimeBooking(Date tBooking) {
+    this.timeBooking = tBooking;
+  }
+
+  public void setTimeBookingString(String sBooking) {
+    this.timeBookingString = sBooking;
+  }
+
+  public void setTeam(String t) {
+    this.vTeam = t;
+  }
+
+  public void setMainSector(String sector) {
+    this.mainSector = sector;
+  }
+
+  public void setMobilePhone(String mPhone) {
+    this.mobilePhone = mPhone;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setNumberOfBookings(int nbrBookings) {
+    this.numberBookings = nbrBookings;
+  }
+
+  public void setMaxNbrBookings(int maxBookings) {
+    this.maxNbrBookings = maxBookings;
+  }
+
+  public String getPnr() {
+    return pnr;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDisplayName() {
+    return displayName;
+  }
+
+  public String getLocalDoctor() {
+    return this.localDoctor;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public Date getTimeBooking() {
+    return this.timeBooking;
+  }
+
+  public String getTimeBookingString() {
+    return this.timeBookingString;
+  }
+
+  public String getTeam() {
+    return this.vTeam;
+  }
+
+  public String getMainSector() {
+
+    return mainSector;
+  }
+
+  public String getMobilePhone() {
+    return mobilePhone;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public int getNumberOfBookings() {
+    return this.numberBookings;
+  }
+
+  public int getMaxNbrBookings() {
+    return this.maxNbrBookings;
+  }
+
+  public boolean getIsDisabled() {
+    return numberBookings == maxNbrBookings;
+  }
+
+  public String toString() {
+    return "pnr " + pnr + " name " + name + " doctor " + localDoctor + " address " + address + " booking " + timeBooking + " team " + vTeam + " main sector " + mainSector + " mobilephone "
+        + mobilePhone + " email " + email + " numberBookings " + numberBookings + " maxNbrBookings " + maxNbrBookings;
+  }
 }
