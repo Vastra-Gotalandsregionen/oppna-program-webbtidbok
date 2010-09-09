3bebda45320757cc50b29892f2b5322c69f06074
carl.stromhielm <carl.stromhielm@localhost>
2010-09-09 09:53:52 +0000

    refactored some in the mail module to make it more modular and also allowed for it to be threaded so that mail requests from users will be put in a queue and the dealt with one of four worker threads.

--- mammografi/screening/composites/webcomp/src/main/java/se/vgregion/webbtidbok/mail/MailSender.java
+++ mammografi/screening/composites/webcomp/src/main/java/se/vgregion/webbtidbok/mail/MailSender.java
@@ -18,29 +18,16 @@
 package se.vgregion.webbtidbok.mail;
 
 import java.util.Properties;
-import java.util.ResourceBundle;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 
 import se.vgregion.webbtidbok.State;
 import se.vgregion.webbtidbok.domain.Booking;
 
 public class MailSender implements Runnable {
-	MailSetup mailSetup = new MailSetup();
-
-	public Properties getMailProperties(State state) {
-
-		Properties properties = mailSetup.setUpMailProperties(state);
-
-		return properties;
-	}
 
 	/**
 	 * Fetches the message subjuect and body from current used message bundle completes the mail subject and mail body with the
@@ -50,98 +37,52 @@ public class MailSender implements Runnable {
 	 * @param state
 	 * @return
 	 */
-	public Message getMessage(Session session, State state, String patientName) {
 
-		ResourceBundle bundle = mailSetup.setUpMailResourceBunle(state);
-		Message msg = new MimeMessage(session);
-		InternetAddress addressFrom = null;
+	private State state;
+	private Booking booking;
 
-		try {
+	public MailSender() {
 
-			addressFrom = new InternetAddress(bundle.getString("fromEmailAddress"));
-
-		} catch (AddressException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			String cancelationAddress = bundle.getString("cancelationToEmailAddress");
-
-			msg.setRecipients(Message.RecipientType.TO, getToAddress(cancelationAddress));
-			msg.setFrom(addressFrom);
-			msg.setSubject(bundle.getString("cancelationMailSubject") + " " + patientName + ", " + state.getPasswd());
-			msg.setContent(bundle.getString("cancelationMailMailBody") + " " + patientName + ", " + state.getPasswd(),
-					"text/plain");
-
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
-
-		return msg;
 	}
 
-	public Session getSession(Properties mailproperties) {
-		final String user = mailproperties.getProperty("userName");
-		final String userKey = mailproperties.getProperty("userKey");
-
-		Session session = Session.getDefaultInstance(mailproperties, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(user, userKey);
-			}
-		});
-
-		return session;
+	public MailSender(State state, Booking booking) {
+		this.state = state;
+		this.booking = booking;
 	}
 
-	public InternetAddress[] getToAddresses(String[] emailaddresses) {
-		InternetAddress[] toAddresses = new InternetAddress[emailaddresses.length];
-		for (int i = 0; i < emailaddresses.length; i++) {
-			try {
-				toAddresses[i] = new InternetAddress(emailaddresses[i]);
-			} catch (AddressException e) {
-				e.printStackTrace();
-			}
-		}
-		return toAddresses;
+	MailSetup mailSetup = new MailSetup();
+	CancellationMessageSetup messageSetup = new CancellationMessageSetup();
+
+	public Properties getMailProperties(State state) {
+
+		Properties properties = mailSetup.setUpMailProperties(state);
+
+		return properties;
 	}
 
-	public InternetAddress[] getToAddress(String emailaddress) {
-		InternetAddress[] toAddress = new InternetAddress[1];
-		try {
-			toAddress[0] = new InternetAddress(emailaddress);
-		} catch (AddressException e) {
-			e.printStackTrace();
-		}
-		return toAddress;
-	}
-
-	public void sendCancelationMail(State state, Booking booking) throws AddressException {
-
-		boolean debug = true;
-		// boolean mailSentToSmtpServer = true;
-		String patientName = booking.getPatientName();
-
-		if (!state.getService().equalsIgnoreCase("BUKAORTA")) {
-			Properties mailProperties = getMailProperties(state);
-			Session session = getSession(mailProperties);
-			session.setDebug(debug);
-			Message messageToSend = getMessage(session, state, patientName);
-
-			try {
-
-				Transport.send(messageToSend);
-				// mailSentToSmtpServer = true;
-
-			} catch (MessagingException e) {
-				e.printStackTrace();
-			}
-		}
-
-		// return mailSentToSmtpServer;
+	public void sendCancellationMail() {
+		run();
 	}
 
 	@Override
 	public void run() {
 
+		boolean debug = true;
+		String patientName = booking.getPatientName();
+
+		if (!state.getService().equalsIgnoreCase("BUKAORTA")) {
+			Properties mailProperties = getMailProperties(state);
+			Session session = mailSetup.getSession(mailProperties);
+			session.setDebug(debug);
+			Message messageToSend = messageSetup.getMessage(session, state, patientName);
+
+			try {
+
+				Transport.send(messageToSend);
+
+			} catch (MessagingException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
