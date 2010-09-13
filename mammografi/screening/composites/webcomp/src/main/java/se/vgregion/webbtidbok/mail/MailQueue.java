237ab96a73e1f25bd2618e251caada49200d3c86
carl.stromhielm <carl.stromhielm@localhost>
2010-09-13 09:25:56 +0000

    changed tests a bit. also concluded that testing results from the random order of events in a thread pool is not easily done.

--- mammografi/screening/composites/webcomp/src/main/java/se/vgregion/webbtidbok/mail/MailQueue.java
+++ mammografi/screening/composites/webcomp/src/main/java/se/vgregion/webbtidbok/mail/MailQueue.java
@@ -30,6 +30,25 @@ public class MailQueue {
 	private int nThreads = 0;
 	private PoolWorker[] threads;
 	private static LinkedList<Runnable> queue;
+	static boolean testModeOn = false;
+	Counter qCounter = new Counter();
+	Counter jobCounter = new Counter();
+
+	/**
+	 * Constructor used by test class. Only difference is the call to setTestMode
+	 */
+	public MailQueue(int nThreads, boolean testMode) {
+		setTestModeOn(testMode);
+		this.nThreads = nThreads;
+		queue = new LinkedList<Runnable>();
+		threads = new PoolWorker[nThreads];
+
+		for (int i = 0; i < nThreads; i++) {
+			threads[i] = new PoolWorker();
+			threads[i].setName(Integer.toString(i));
+			threads[i].start();
+		}
+	}
 
 	public MailQueue(int nThreads) {
 		this.nThreads = nThreads;
@@ -38,11 +57,16 @@ public class MailQueue {
 
 		for (int i = 0; i < nThreads; i++) {
 			threads[i] = new PoolWorker();
+			threads[i].setName(Integer.toString(i));
 			threads[i].start();
 		}
 	}
 
 	public void execute(Runnable r) {
+		synchronized (qCounter) {
+			qCounter.increment();
+			System.out.println("Jobs put into queue: " + qCounter.getNJobValue());
+		}
 
 		synchronized (queue) {
 			queue.addLast(r);
@@ -53,20 +77,39 @@ public class MailQueue {
 	private class PoolWorker extends Thread {
 
 		public void run() {
+
 			Runnable job = null;
+
 			while (true) {
 				synchronized (queue) {
+
 					while (queue.isEmpty()) {
 						try {
+
 							queue.wait();
+
 						} catch (InterruptedException e) {
 							e.printStackTrace();
 						}
 					}
+
 					job = (Runnable) queue.removeFirst();
+
 				}
 				try {
-					job.run();
+					if (testModeOn == false) {
+
+						job.run();
+
+					} else if (testModeOn == true) {
+
+						Thread ct = Thread.currentThread();
+						System.out.println("ct: " + ct.getName());
+						synchronized (jobCounter) {
+							jobCounter.increment();
+							System.out.println("Job's run: " + jobCounter.getNJobValue());
+						}
+					}
 
 				} catch (RuntimeException e) {
 					e.printStackTrace();
@@ -74,4 +117,25 @@ public class MailQueue {
 			}
 		}
 	}
+
+	public final class Counter {
+		private int nJob = 0;
+
+		public synchronized int getNJobValue() {
+			return nJob;
+		}
+
+		public synchronized int increment() {
+			return ++nJob;
+		}
+	}
+
+	public static boolean isTestModeOn() {
+		return testModeOn;
+	}
+
+	public static void setTestModeOn(boolean testModeOn) {
+		MailQueue.testModeOn = testModeOn;
+	}
+
 }
