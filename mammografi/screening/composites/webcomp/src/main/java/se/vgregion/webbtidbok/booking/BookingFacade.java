8aa35e026f21b931c1fb4ed6206ed4af89928495
anders henriksson <anders.henriksson@knowit.se>
2010-10-01 16:34:03 +0000

    Comments for the BookingFacade interface.

--- mammografi/screening/composites/webcomp/src/main/java/se/vgregion/webbtidbok/booking/BookingFacade.java
+++ mammografi/screening/composites/webcomp/src/main/java/se/vgregion/webbtidbok/booking/BookingFacade.java
@@ -25,10 +25,22 @@ import se.vgregion.webbtidbok.domain.Booking;
 import se.vgregion.webbtidbok.domain.BookingTime;
 import se.vgregion.webbtidbok.domain.Surgery;
 
+/**
+ * The interface towards the underlying booking systems.
+ * 
+ * When implementing a backend for a new booking system, this is the interface to use.
+ * All methods have to be implemented, and all objects crossing this boundary have to 
+ * be of the Webbtidbok generic version. The webbtidbok should not use any methods
+ * of underlying service implementations.
+ *
+ */
 public interface BookingFacade {
 
 	/**
 	 * Login to the booking system.
+	 * 
+	 * This call has to be implemented even if the system does not require login prior
+	 * to accepting other calls. (Such as if authentication is done on all requests.)
 	 * 
 	 * @param state
 	 *            The session state used to store the login credentials.
@@ -36,16 +48,65 @@ public interface BookingFacade {
 	 */
 	boolean login(State state);
 
+	/**
+	 * Get information on the user's current booking.
+	 * 
+	 * @param state The state needs to contain valid login credentials. (I.e. the users
+	 *              should have logged in.
+	 * @return A {@link Booking} object holding all the information needed by the UI.
+	 */
 	Booking getBookingInfo(State state);
 
+	/**
+	 * Reschedule the user's appointment.
+	 * 
+	 * @param bookingTime The new time chosen by the user.
+     * @param state The state needs to contain valid login credentials. (I.e. the users
+     *              should have logged in.)
+	 */
 	void reschedule(BookingTime bookingTime, State state);
 
+	/**
+	 * Lists all available surgeries for the relevant procedure.
+	 * Note that this list does not cross web service boundaries, so you only get surgeries
+	 * available from the same domain as the user is currently booked at.
+	 * 
+     * @param state The state needs to contain valid login credentials. (I.e. the users
+     *              should have logged in.)
+	 * @return A list of all available {@link Surgery}.
+	 */
 	List<Surgery> getAvailableSurgeries(State state);
 
+	/**
+	 * Find all days having available times for the selected surgery in the given time span.
+	 * 
+     * @param state The state needs to contain valid login credentials. (I.e. the users
+     *              should have logged in.)
+	 * @param surgeryId The {@link Surgery} to list times for.
+	 * @param startDate First date in period, inclusive.
+	 * @param endDate Last date in period, inclusive.
+	 * @return A list of days where available times were found.
+	 */
 	List<Calendar> getFreeDays(State state, String surgeryId, Calendar startDate, Calendar endDate);
 
+	/**
+	 * Get all available times for a given date and surgery.
+	 * 
+     * @param state The state needs to contain valid login credentials. (I.e. the users
+     *              should have logged in.)
+	 * @param sectionId The {@link Surgery} to list times for.
+	 * @param selectedDate The date to list times for.
+	 * @return A list of available times.
+	 */
 	List<BookingTime> getBookingTime(State state, String sectionId, Calendar selectedDate);
 
+	/**
+	 * Cancel the booking for the user.
+	 * 
+     * @param state The state needs to contain valid login credentials. (I.e. the users
+     *              should have logged in.)
+	 * @return true if the booking was properly canceled.
+	 */
 	boolean cancelBooking(State state);
 
 }
